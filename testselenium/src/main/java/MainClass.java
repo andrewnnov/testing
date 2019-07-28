678fe4a857829a29c7ebbc0439cc516da5db8035
andreybannikov <andrewnnov@yandex.ru>
2019-07-28 19:57:10 +0300

    simple method

--- testselenium/src/main/java/MainClass.java
+++ testselenium/src/main/java/MainClass.java
@@ -1,11 +1,24 @@
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class MainClass {
     public static void main(String[] args) {
         System.setProperty("webdriver.gecko.driver", "C:\\Projects\\testing\\geckodriver-v0.24.0-win64\\geckodriver.exe");
         WebDriver driver = new FirefoxDriver();
+        driver.manage().window().setSize(new Dimension(900, 500));
         driver.get("https://www.seleniumhq.org/");
+        driver.navigate().to("https://www.google.com");
+        driver.navigate().back();
+        driver.navigate().forward();
+        driver.navigate().refresh();
+        System.out.println(driver.getTitle());
+        System.out.println(driver.getCurrentUrl());
+        driver.quit();
+
+
+
     }
 
 }
