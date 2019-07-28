58152cb97666a11532c8aec24be66c557956064f
andreybannikov <andrewnnov@yandex.ru>
2019-07-28 20:01:35 +0300

    implicit wait

--- testselenium/src/main/java/MainClass.java
+++ testselenium/src/main/java/MainClass.java
@@ -3,10 +3,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.util.concurrent.TimeUnit;
+
 public class MainClass {
     public static void main(String[] args) {
         System.setProperty("webdriver.gecko.driver", "C:\\Projects\\testing\\geckodriver-v0.24.0-win64\\geckodriver.exe");
         WebDriver driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().setSize(new Dimension(900, 500));
         driver.get("https://www.seleniumhq.org/");
         driver.navigate().to("https://www.google.com");
