10865a8c5fcba50d72dc74dba644002200d28a3d
andreybannikov <andrewnnov@yandex.ru>
2019-07-30 20:44:30 +0300

    work with button

--- testselenium/src/main/java/MainClass.java
+++ testselenium/src/main/java/MainClass.java
@@ -13,19 +13,21 @@ public class MainClass {
         WebDriver driver = new FirefoxDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().maximize();
-        driver.get("https://ru.wikipedia.org");
-        WebElement link = driver.findElement(By.linkText("Войти"));
-        System.out.println(link);
-        WebElement element = driver.findElement(By.partialLinkText("Свежие"));
-        System.out.println(element.getText());
-        WebElement searchField = driver.findElement(By.name("search"));
-        WebElement searchButton = driver.findElement(By.className("searchButton"));
-        WebElement idElement = driver.findElement(By.id("ca-viewsource"));
-        WebElement tagName = driver.findElement(By.tagName("input")); //return first element on page
-        WebElement elementCss = driver.findElement(By.cssSelector("#simpleSearch"));
-        WebElement elementXPath = driver.findElement(By.xpath("/html/body/div[4]/div[2]/div[1]/a"));
+        //driver.get("https://ru.wikipedia.org");
+        //driver.findElement(By.xpath("//*[@id=\"searchButton\"]")).click();
 
-        driver.quit();
+        driver.get("https://github.com/");
+        WebElement elementButton = driver.findElement(By.xpath("/html/body/div[4]/main/div[1]/div/div/div[2]/div/form/button"));
+        if(elementButton.getText().equals("Sign up for GitHub")) {
+            System.out.println("Success");
+        } else {
+            System.out.println("Fail");
+        }
+
+        System.out.println("text button is: " + elementButton.getText());
+        elementButton.submit();
+
+        //driver.quit();
 
 
 
