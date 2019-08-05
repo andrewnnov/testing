9de4ae995c4b700fe20bad17c0f143cf6212b22e
andreybannikov <andrewnnov@yandex.ru>
2019-08-05 22:28:46 +0300

    work with drop down list

--- testselenium/src/main/java/MainClass.java
+++ testselenium/src/main/java/MainClass.java
@@ -13,20 +13,57 @@ public class MainClass {
         WebDriver driver = new FirefoxDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().maximize();
-        driver.get("https://en.wikipedia.org");
 
-        WebElement link = driver.findElement(By.xpath("/html/body/div[4]/div[2]/div[3]/div/ul/li[2]/a"));
+        driver.get("https://gsuite.google.com/signup/basic/welcome");
+        driver.findElement(By.xpath("/html/body/c-wiz/div[2]/div[2]/section/div/div[1]/div[1]/c-wiz[5]/div/div[2]/div[1]/div[1]/div[1]/div[1]")).click();
+        driver.findElement(By.xpath("/html/body/c-wiz/div[2]/div[2]/section/div/div[1]/div[1]/c-wiz[5]/div/div[2]/div[1]/div[2]/div[6]/span")).click();
 
-        System.out.println(link.getText());
-        link.click();
 
-        driver.get("https://www.facebook.com");
-        WebElement linkFB = driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div/div/div/div[2]/form/table/tbody/tr[3]/td[2]/div/a"));
-        linkFB.click();
+        //driver.get("https://market.yandex.ru/");
+
+
+//        driver.findElement(By.xpath("/html/body/div[1]/div/span/div[2]/noindex/div[2]/div/div/div/div[3]/a/span")).click();
+//        driver.findElement(By.xpath("/html/body/div[1]/div[2]/div[7]/div/div/div[1]/div/div/div/div/div/div/div[2]" +
+//                "/div[2]/ul/li[1]/div/a")).click();
+//
+//        //проверка выделен ли чек бокс
+//        System.out.println(driver.findElement(By.xpath("//label[text()='Производитель ATLANT']/preceding-sibling::span/input")).isSelected());
+//        //веделяем чек бокс
+//        driver.findElement(By.xpath("//label[text()='ATLANT']/preceding-sibling::span/input")).click();
+//
+//        //снова проееряем чекбокс
+//        System.out.println(driver.findElement(By.xpath("//label[text()='ATLANT']/preceding-sibling::span/input")).isSelected());
+//
+//
+//
+//
+//        driver.findElement(By.xpath("/html/body/div[1]/div[5]/div[2]/div[2]/div/div/div/div[3]/div/div[2]/div[3]/fieldset/ul/li[2]/div/a/label/div")).click();
+//
+//        driver.findElement(By.xpath("/html/body/div[1]/div[5]/div[2]/div[2]/div/div/div/div[3]/div/div[2]/div[3]/fieldset/ul/li[3]/div/a/label/div")).click();
+//
+//
+//        System.out.println(driver.findElement(By.xpath("/html/body/div[1]/div[5]/div[2]/div[2]/div/div/div/div[3]/div/div[2]/div[22]/fieldset/ul/li[1]/div/label/div")).isSelected());
+//
+//        driver.findElement(By.xpath("/html/body/div[1]/div[5]/div[2]/div[2]/div/div/div/div[3]/div/div[2]/div[22]/fieldset/ul/li[1]/div/label/div")).click();
+//
+//        System.out.println(driver.findElement(By.xpath("/html/body/div[1]/div[5]/div[2]/div[2]/div/div/div/div[3]/div/div[2]/div[22]/fieldset/ul/li[1]/div/label/div")).isSelected());
+//
+//
+//        driver.findElement(By.xpath("/html/body/div[1]/div[5]/div[2]/div[2]/div/div/div/div[3]/div/div[2]/div[22]/fieldset/ul/li[2]/div/label/div")).click();
+
+
+
+
+
+
 
 
 
         //driver.quit();
+
+
+
+
 
 
 
